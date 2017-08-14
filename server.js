584f7d54837d873f7515e310251a44f77b429aa1
SID-PC\SiD <siddharth.nagrajann@gmail.com>
2017-08-14 18:29:39 +0530

    removed maintainance script

--- server.js
+++ server.js
@@ -23,9 +23,9 @@ app.use((req,res,next)=>{
 });
 
 
-app.use((req,res,next)=>{
-    res.render('maintain.hbs');
-});
+// app.use((req,res,next)=>{
+//     res.render('maintain.hbs');
+// });
 
 app.use(express.static(__dirname + "/public"));
 
